5f318b68993d6542c72278bfbddc4bc8153bd985
Delil Noyan <noyandelil1903@hotmail.com>
2017-12-04 13:18:33 +0300

    ListBox İşlemler

---
+++ Binode/WindowsFormIlkProje/Form5.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormIlkProje
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTcKimlikNo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUzmanlik = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstKisiler = new System.Windows.Forms.ListBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(87, 16);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(126, 20);
+            this.txtAd.TabIndex = 1;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(87, 65);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(126, 20);
+            this.txtSoyad.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Soyad";
+            // 
+            // txtTcKimlikNo
+            // 
+            this.txtTcKimlikNo.Location = new System.Drawing.Point(87, 113);
+            this.txtTcKimlikNo.Name = "txtTcKimlikNo";
+            this.txtTcKimlikNo.Size = new System.Drawing.Size(126, 20);
+            this.txtTcKimlikNo.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "TC Kimlik";
+            // 
+            // txtUzmanlik
+            // 
+            this.txtUzmanlik.Location = new System.Drawing.Point(87, 167);
+            this.txtUzmanlik.Name = "txtUzmanlik";
+            this.txtUzmanlik.Size = new System.Drawing.Size(126, 20);
+            this.txtUzmanlik.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Uzmanlik";
+            // 
+            // lstKisiler
+            // 
+            this.lstKisiler.FormattingEnabled = true;
+            this.lstKisiler.Location = new System.Drawing.Point(228, 16);
+            this.lstKisiler.Name = "lstKisiler";
+            this.lstKisiler.Size = new System.Drawing.Size(287, 251);
+            this.lstKisiler.TabIndex = 8;
+            this.lstKisiler.SelectedIndexChanged += new System.EventHandler(this.lstKisiler_SelectedIndexChanged);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(16, 227);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 9;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(138, 227);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 10;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 320);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.lstKisiler);
+            this.Controls.Add(this.txtUzmanlik);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTcKimlikNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label1);
+            this.Name = "Form5";
+            this.Text = "Form5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTcKimlikNo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUzmanlik;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstKisiler;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnKaydet;
+    }
+}
---
+++ Binode/WindowsFormIlkProje/Form5.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormIlkProje
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private bool isUpdate;
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            Kisi kisi = (isUpdate) ? (Kisi)lstKisiler.SelectedItem : new Kisi();
+
+            kisi.Ad = txtAd.Text;
+            kisi.Soyad = txtSoyad.Text;
+            kisi.TcKimlikNo = txtTcKimlikNo.Text;
+            kisi.Uzmanlik = txtUzmanlik.Text;
+
+            if (isUpdate)
+            {
+                lstKisiler.Items[lstKisiler.SelectedIndex] = kisi;
+                btnEkle.Text = "Ekle";
+            }
+            else
+                lstKisiler.Items.Add(kisi);
+
+            txtAd.Text = "";
+            txtSoyad.Text = "";
+            txtTcKimlikNo.Text = "";
+            txtUzmanlik.Text = "";
+
+            isUpdate = false;
+        }
+
+        private void lstKisiler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Kisi gelenKisi = lstKisiler.SelectedItem as Kisi;
+
+            if (gelenKisi != null)
+            {
+                isUpdate = true;
+                btnEkle.Text = "Güncelle";
+                txtAd.Text = gelenKisi.Ad;
+                txtSoyad.Text = gelenKisi.Soyad;
+                txtTcKimlikNo.Text = gelenKisi.TcKimlikNo;
+                txtUzmanlik.Text = gelenKisi.Uzmanlik;
+            }
+
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            var seciliItem = lstKisiler.SelectedItem;
+            if (seciliItem != null)
+            {
+                Kisi gelenKisi = seciliItem as Kisi;
+
+                int seciliIndex = lstKisiler.SelectedIndex;
+
+                lstKisiler.Items.RemoveAt(seciliIndex);
+
+                gelenKisi.Ad = txtAd.Text;
+                gelenKisi.Soyad = txtSoyad.Text;
+                gelenKisi.TcKimlikNo = txtTcKimlikNo.Text;
+                gelenKisi.Uzmanlik = txtUzmanlik.Text;
+
+                lstKisiler.Items.Insert(seciliIndex, gelenKisi);
+            }
+            else
+                MessageBox.Show("Lütfen Güncellenecek Kişiyi Seçiniz!!");
+        }
+    }
+}
